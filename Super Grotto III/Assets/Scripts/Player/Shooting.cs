77712bdd8178ba885f267733931e185368bae56d
Vasanth2801 <vasanthravichandran28@gmail.com>
2026-04-07 10:30:51 +0530

    Shooting and bullet Script

---
+++ Super Grotto III/Assets/Scripts/Player/Shooting.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    [Header("Shooting Settings")]
+    [SerializeField] private float bulletForce = 20f;
+
+    [Header("References")]
+    [SerializeField] private Transform firePoint;
+    [SerializeField] private ObjectPooler pooler;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.K))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject bullet = pooler.SpawnFromPools("Bullet", firePoint.position,firePoint.rotation);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(transform.right * bulletForce, ForceMode2D.Impulse);
+    }
+}
