b919d61f77b503334bafdac1638c3e275bf014ba
Vasanth2801 <vasanthravichandran28@gmail.com>
2026-04-07 18:58:45 +0530

    Scene Loading from one to another

--- Super Grotto III/Assets/Scripts/UI/SceneLoader.cs
+++ Super Grotto III/Assets/Scripts/UI/SceneLoader.cs
@@ -3,5 +3,11 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-    
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+        }
+    }
 }
